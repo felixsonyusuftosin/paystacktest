9aca3a73b2a3468bd4b3fb76ef183bf1f9922911
felixsonyusuftosin <felixsonyusuftosin@gmail.com>
2019-10-25 18:34:44 +0100

    remove logging from production and set up webpack to respect .env

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,9 +2,31 @@
  * @file here is a very goood place to log errors to a logging service like sentry
  */
 
+const ENVIRONMENT = process.env.NODE_ENV
+
 export const logger = {
-	error: err => console.error(err),
-	warn: warning => console.warn(warning),
-	message: message => console.log(message),
-	dir: directory => console.dir(directory)
+	error: err => {
+		// you can implement method to right to a logger service here
+		if (ENVIRONMENT === 'development') {
+			console.error(err)
+		}
+	},
+	warn: warning => {
+		// you can implement method to right to a logger service here
+		if (ENVIRONMENT === 'development') {
+			console.warn(warning)
+		}
+	},
+	message: message => {
+		// you can implement method to right to a logger service here
+		if (ENVIRONMENT === 'development') {
+			console.log(message)
+		}
+	},
+	dir: directory => {
+		// you can implement method to right to a logger service here
+		if (ENVIRONMENT === 'development') {
+			console.dir(directory)
+		}
+	}
 }
