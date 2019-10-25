79860ec223dcca3e1dfa67a6255904644942c5a7
felixsonyusuftosin <felixsonyusuftosin@gmail.com>
2019-10-25 12:50:15 +0100

    update the app to remove pagination

--- src/components/home/table/index.jsx
+++ src/components/home/table/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Select as SelectDropDown } from 'react-dropdown-select'
-import { Pagination, Loader } from '@widgets'
-import { useSelector, shallowEqual, useDispatch } from 'react-redux'
+import { Loader } from '@widgets'
+import { useSelector, useDispatch } from 'react-redux'
 import { filterByGender, sortActorsByName, sortActorsByHeight } from '@utils'
 import { dispatchActionsSync } from '@store'
 import '@home/table/styles/table.scss'
@@ -11,7 +11,7 @@ const genderIcon = {
 	female: 'female',
 	hermaphrodite: 'meh-o',
 	'n/a': 'question',
-	none: 'question'
+	none: 'question-circle'
 }
 const genderOptions = [
 	{ label: 'Male', value: 'male' },
@@ -24,32 +24,29 @@ const genderOptions = [
 export const Table = () => {
 	const dispatch = useDispatch()
 
-	const [localActorsList, setLocalActorsList] = useState([])
-
 	let totalHeightCm = 0
 	let totalHeightIn = 0
 	let totalHeightft = 0
 
-	if (localActorsList.length) {
-		const reduced = localActorsList.reduce((total, currentValue) => {
+	const actorsPending = useSelector(state => state.actorsList.pending)
+	const actorsError = useSelector(state => state.actorsList.error)
+	const actors = useSelector(state =>
+		state.actorsList.payload ? state.actorsList.payload.characters : null
+	)
+
+	if (actors && actors.length) {
+		const reduced = actors.reduce((total, currentValue) => {
 			const height = isNaN(+currentValue.height) ? 0 : +currentValue.height
+
 			return total + height
 		}, 0)
+
 		totalHeightCm = reduced
 		totalHeightIn = parseInt(reduced / 25.4, 10)
 		totalHeightft = parseInt(reduced / 30.4, 10)
 	}
 
-	const actorsPending = useSelector(
-		state => state.actorsList.pending,
-		shallowEqual
-	)
-
-	const actorsError = useSelector(state => state.actorsList.error)
-
-	const actors = useSelector(state =>
-		state.actorsList ? state.actorsList.payload : null
-	)
+	const formatComputedHeight = `${totalHeightCm}cm (${totalHeightft}ft/${totalHeightIn}in)`
 
 	const fullActorsList = useSelector(state =>
 		state.fullActorsList ? state.fullActorsList.payload : null
@@ -69,6 +66,7 @@ export const Table = () => {
 				order === 'ascending'
 			)
 			const sortedActorsFilm = { ...actors, characters: sortedActors }
+
 			dispatch(dispatchActionsSync('ACTORS_LIST', sortedActorsFilm))
 			setSorting({ name: 'name', order })
 		}
@@ -83,18 +81,20 @@ export const Table = () => {
 				order === 'ascending'
 			)
 			const sortedActorsFilm = { ...actors, characters: sortedActors }
+
 			dispatch(dispatchActionsSync('ACTORS_LIST', sortedActorsFilm))
 			setSorting({ name: 'height', order })
 		}
 	}
 
 	const onFilterByGender = gender => {
-		if (fullActorsList && actors && gender && gender.length) {
+		if (gender.length) {
 			const filteredActors = filterByGender(
 				gender[0].value,
 				fullActorsList.characters
 			)
 			const filteredActorsFilm = { ...actors, characters: filteredActors }
+
 			dispatch(dispatchActionsSync('ACTORS_LIST', filteredActorsFilm))
 		}
 	}
@@ -103,95 +103,106 @@ export const Table = () => {
 		dispatch(dispatchActionsSync('ACTORS_LIST', fullActorsList))
 	}
 
-	return (
-		<Pagination onActorsLoaded={response => setLocalActorsList(response)}>
-			<section className="table">
-				{localActorsList.length ? (
-					<div className="row table-header">
-						<div className="column">
-							<span> Gender</span>
-							<div className="select-bar">
-								<SelectDropDown
-									options={genderOptions}
-									labelField="label"
-									valueField="value"
-									onClearAll={clearGenderFilter}
-									onChange={onFilterByGender}
-									clearable={true}
-								/>
-							</div>
-						</div>
-						<div className="column" onClick={sortTableByName}>
-							<span>
-								Name{' '}
-								{sortParameter.name === 'name' &&
-								sortParameter.order === 'ascending' ? (
-									<i className="fa fa-arrow-up" />
-								) : (
-									sortParameter.name === 'name' && (
-										<i className="fa fa-arrow-down" />
-									)
-								)}
-							</span>
-						</div>
-						<div className="column" onClick={sortTableByHeight}>
-							<span>
-								Height{' '}
-								{sortParameter.name === 'height' &&
-								sortParameter.order === 'ascending' ? (
-									<i className="fa fa-arrow-up" />
-								) : (
-									sortParameter.name === 'height' && (
-										<i className="fa fa-arrow-down" />
-									)
-								)}
-							</span>
-						</div>
-					</div>
-				) : null}
-				{actors && localActorsList.length && !actorsPending
-					? localActorsList.map((actor, index) => (
-							<div className="row" key={index}>
-								<div className="column">
-									<i className={`fa fa-${genderIcon[actor.gender]}`}></i>{' '}
-								</div>
-								<div className="column"> {actor.name}</div>
-								<div className="column"> {actor.height} </div>
-							</div>
-					  ))
-					: null}
-				{actors && localActorsList.length && !actorsPending && (
-					<div className="row footer">
-						<div className="column"></div>
-						<div className="column">
-							{' '}
-							{localActorsList.length} &nbsp; Actors{' '}
-						</div>
-						<div className="column">{`${totalHeightCm}cm (${totalHeightft}ft/${totalHeightIn}in)`}</div>
-					</div>
-				)}
+	const componentUiState = {
+		actorsNotEmpty: actors && actors.length,
+		isAscendingSortByName:
+			sortParameter === 'name' && sortParameter.order === 'ascending',
+		isDescendingSortByName:
+			sortParameter === 'name' && sortParameter.order === 'descending',
+		isAscendingSortByHeight:
+			sortParameter === 'height' && sortParameter.order === 'ascending',
+		isDescendingSortByHeight:
+			sortParameter === 'height' && sortParameter.order === 'descending',
+		actorsListIsReady: actors && !actorsPending && !actorsError,
+		noMovieSelected: !actors && !actorsPending && !actorsError,
+		actorsListIsLoading: !actors && !actorsError && actorsPending,
+		actorsListErroredWhileFetching: !actors && !actorsPending && actorsError
+	}
 
-				{(!localActorsList.length && !actorsPending) ||
-				(!actors && !actorsPending) ? (
-					<div className="row">
-						<h3 className="no-data">
-							Please select a movie to see the list of actors
-						</h3>
+	const {
+		actorsNotEmpty,
+		isAscendingSortByHeight,
+		isAscendingSortByName,
+		isDescendingSortByHeight,
+		isDescendingSortByName,
+		actorsListIsReady,
+		noMovieSelected,
+		actorsListIsLoading,
+		actorsListErroredWhileFetching
+	} = componentUiState
+
+	return (
+		<section className="table">
+			{actorsNotEmpty && (
+				<div className="row table-header">
+					<div className="column">
+						<span> Gender</span>
+						<div className="select-bar">
+							<SelectDropDown
+								options={genderOptions}
+								labelField="label"
+								valueField="value"
+								onClearAll={clearGenderFilter}
+								onChange={onFilterByGender}
+								clearable={true}
+							/>
+						</div>
 					</div>
-				) : null}
-				{actorsPending && !actorsError && (
-					<div className="row laoder">
-						<Loader />
+					<div className="column" onClick={sortTableByName}>
+						<span>
+							Name {isAscendingSortByName && <i className="fa fa-arrow-up" />}
+							{isDescendingSortByName && <i className="fa fa-arrow-down" />}
+						</span>
 					</div>
-				)}
-				{actorsError && (
-					<div className="row">
-						<h3 className="no-data">
-							Please select a movie to see the list of actors
-						</h3>
+					<div className="column" onClick={sortTableByHeight}>
+						<span>
+							Height{' '}
+							{isAscendingSortByHeight && <i className="fa fa-arrow-up" />}
+							{isDescendingSortByHeight && <i className="fa fa-arrow-down" />}
+						</span>
 					</div>
-				)}
-			</section>
-		</Pagination>
+				</div>
+			)}
+
+			{actorsListIsReady
+				? actors.map((actor, index) => (
+						<div className="row" key={index}>
+							<div className="column">
+								<i className={`fa fa-${genderIcon[actor.gender]}`}></i>{' '}
+							</div>
+							<div className="column"> {actor.name}</div>
+							<div className="column"> {actor.height} </div>
+						</div>
+				  ))
+				: null}
+
+			{actorsListIsReady && (
+				<div className="row footer">
+					<div className="column"></div>
+					<div className="column"> {actors.length} &nbsp; Actors </div>
+					<div className="column">{formatComputedHeight}</div>
+				</div>
+			)}
+
+			{noMovieSelected ? (
+				<div className="row">
+					<h3 className="no-data">
+						Please select a movie to see the list of actors
+					</h3>
+				</div>
+			) : null}
+
+			{actorsListIsLoading && (
+				<div className="row laoder">
+					<Loader />
+				</div>
+			)}
+
+			{actorsListErroredWhileFetching && (
+				<div className="row">
+					<h3 className="no-data">{actorsError}</h3>
+				</div>
+			)}
+		</section>
 	)
 }
